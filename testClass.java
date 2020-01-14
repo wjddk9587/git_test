22bf412bcc97a6c84e1c5d5ccf0348a15dcecac0
wjddk9587 <wjddk9587@naver.com>
2020-01-14 15:19:19 +0900

    second commit in branch

--- testClass.java
+++ testClass.java
@@ -3,6 +3,7 @@ package test;
 public class testClass{
     public static void main(String[] args){
         System.out.println("I miss bro!!");
+        System.out.println("I love bro!");
     }
 
 }
